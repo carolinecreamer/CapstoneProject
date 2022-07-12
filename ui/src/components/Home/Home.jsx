c1c98458233cdca6f4c73c22e9ebe15060cdc1c3
Caroline Creamer <ccreamer@fb.com>
2022-07-12 15:39:23 -0700

    Map Layout

--- ui/src/components/Home/Home.jsx
+++ ui/src/components/Home/Home.jsx
@@ -1,16 +1,19 @@
 import * as React from "react"
-import MessagesView from "../MessagesView/MessagesView";
+import Map from "../Map/Map"
 import LoggedOutView from "../LoggedOutView/LoggedOutView";
 import * as Bootstrap from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Home.css"
 
 export default function Home({ isLoggedIn, handleLogout, handleLogin }) {
+<<<<<<< HEAD
     // If the user is logged in, go to the home page, otherwise show the login screen
+=======
+>>>>>>> ce97652 (Map Layout)
     return (
         <Bootstrap.Container className="homepage">
             {isLoggedIn
-                ? <MessagesView />
+                ? <Map />
                 : <LoggedOutView handleLogin={handleLogin} />
               }
         </Bootstrap.Container>
