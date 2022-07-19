e78572761839c7c856f99000dd133db9cbcc798e
Caroline Creamer <ccreamer@fb.com>
2022-07-18 17:12:41 -0700

    added Zoomable Group so that Map can be zoomed in on

--- ui/src/components/Map/Map.jsx
+++ ui/src/components/Map/Map.jsx
@@ -7,7 +7,8 @@ import {
   Geographies,
   Geography,
   Marker,
-  Annotation
+  Annotation,
+  ZoomableGroup
 } from "react-simple-maps";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Map.css'
@@ -38,68 +39,72 @@ const Map = () => {
   const [starred, setStarred] = useState(false);
   return (
     <ComposableMap projection="geoAlbers">
-      <Geographies geography={geoUrl}>
-        {({ geographies }) => (
-          <>
-            {/* Maps over each state to create a Geography object for each state -> Geography object is the state image on the map */}
-            {geographies.map(geo => (
-              <Geography
-                key={geo.rsmKey}
-                stroke="#FFF"
-                geography={geo}
-                fill="#DDD"
-                className="state"
-              />
-            ))}
-            {/* Maps over each state to add a label to each state*/}
-            {geographies.map(geo => {
-              const centroid = geoCentroid(geo);
-              const cur = allStates.find(s => s.val === geo.id);
+      <ZoomableGroup>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) => (
+            <>
+              {/* Maps over each state to create a Geography object for each state -> Geography object is the state image on the map */}
+              {geographies.map(geo => (
+                <Geography
+                  key={geo.rsmKey}
+                  stroke="#FFF"
+                  geography={geo}
+                  fill="#DDD"
+                  className="state"
+                />
+              ))}
+              {/* Maps over each state to add a label to each state*/}
+              {geographies.map(geo => {
+                const centroid = geoCentroid(geo);
+                const cur = allStates.find(s => s.val === geo.id);
+                return (
+                  <g key={geo.rsmKey + "-name"}>
+                    {
+                      cur &&
+                      centroid[0] > MIN_US_LONGITUDE &&
+                      centroid[0] < MAX_US_LONGITUDE &&
+                      (cur.id in Object.keys(offsets) ? (
+                        <Annotation
+                          subject={centroid}
+                          dx={offsets[cur.id][0]}
+                          dy={offsets[cur.id][1]}
+                        >
+
+
+                          <text x={4} fontSize={14} alignmentBaseline="middle">
+                            {cur.id}
+                          </text>
+
+                        </Annotation>
+                      ) : (
+                        <Marker coordinates={centroid}>
+                          <text y="2" fontSize={14} textAnchor="middle">
+                            {cur.id}
+                          </text>
+                        </Marker>
+                      )
+                      )}
+                  </g>
+                );
+              })}
+            </>
+          )}
+        </Geographies>
+
+        {
+          // Maps over each city in each state to add a marker on its location. The markers are encapsulated by an Overlay Trigger so that a
+          // popup window appears with info about the city, when the marker is clicked on
+          states.map((state) => (
+            state.cities.map(({ name, coordinates }) => {
               return (
-                <g key={geo.rsmKey + "-name"}>
-                  {cur &&
-                    centroid[0] > MIN_US_LONGITUDE &&
-                    centroid[0] < MAX_US_LONGITUDE &&
-                    (Object.keys(offsets).indexOf(cur.id) === -1 ? (
-                      <Marker coordinates={centroid}>
-                        <text y="2" fontSize={14} textAnchor="middle">
-                          {cur.id}
-                        </text>
-                      </Marker>
-                    ) : (
-                      <Annotation
-                        subject={centroid}
-                        dx={offsets[cur.id][0]}
-                        dy={offsets[cur.id][1]}
-                      >
-
-
-                        <text x={4} fontSize={14} alignmentBaseline="middle">
-                          {cur.id}
-                        </text>
-
-                      </Annotation>
-                    ))}
-                </g>
-              );
-            })}
-          </>
-        )}
-      </Geographies>
-
-      {
-        // Maps over each city in each state to add a marker on its location. The markers are encapsulated by an Overlay Trigger so that a
-        // popup window appears with info about the city, when the marker is clicked on
-        states.map((state) => (
-          state.cities.map(({ name, coordinates }) => {
-            return (
-              <Marker key={name} coordinates={coordinates}>
-                <PopoverTrigger name={name}/>
-              </Marker>)
-          })
-        )
-        )
-      }
+                <Marker key={name} coordinates={coordinates}>
+                  <PopoverTrigger name={name} />
+                </Marker>)
+            })
+          )
+          )
+        }
+      </ZoomableGroup>
     </ComposableMap>
   );
 };
