1bb9d392cd189dcb3ccfc1f8131cbd6a03297a03
Caroline Creamer <ccreamer@fb.com>
2022-07-22 11:31:42 -0700

    Display users in feed

--- ui/src/components/Feed/Feed.jsx
+++ ui/src/components/Feed/Feed.jsx
@@ -4,12 +4,26 @@ import LoggedOutView from "../LoggedOutView/LoggedOutView";
 import * as Bootstrap from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Feed.css"
+import { BsConeStriped } from "react-icons/bs";
 
-export default function Feed({currentUser}) {
+export default function Feed({currentUser, getUsers, users}) {
+    React.useEffect(() => {
+        getUsers();
+    }, [])
 
+    // Display other users that use the web page
     return (
         <Bootstrap.Container className="homepage">
-            hi {currentUser}
+            <Bootstrap.ListGroup>
+
+                {users.map((user) => {
+                    if (user?.username != currentUser?.username) {
+                        return (
+                            <Bootstrap.ListGroupItem>{user?.username}</Bootstrap.ListGroupItem>
+                        )
+                    }
+                })}
+            </Bootstrap.ListGroup>
         </Bootstrap.Container>
     )
 }
