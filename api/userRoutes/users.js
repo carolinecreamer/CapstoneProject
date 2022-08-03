14fd1b6fc401928e7c1fab55c0f7c48321f4fc2b
Caroline Creamer <ccreamer@fb.com>
2022-08-02 18:25:04 -0700

    working popovers with complete listings

--- api/userRoutes/users.js
+++ api/userRoutes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router  = express.Router();
-const User  = require("../models/models")
+const User  = require("../citiesModels/cities")
 const Parse = require('parse/node')
 
 
