bf092ab73994185076b5c22fd74b9bf0432c6f10
carolinecreamer <35511922+carolinecreamer@users.noreply.github.com>
2022-08-03 10:18:15 -0700

    Update Home.jsx

--- ui/src/components/Home/Home.jsx
+++ ui/src/components/Home/Home.jsx
@@ -4,16 +4,14 @@ import LoggedOutView from "../LoggedOutView/LoggedOutView";
 import * as Bootstrap from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Home.css"
-import Load from "../Load/Load";
 
-export default function Home({ queryCityFromDB, following, setCities, isLoggedIn, handleLogin, cities, getCities, currentUser, getFollowing, setLoading, setFollowing, friendFavorites }) {
+export default function Home({ isLoggedIn, handleLogout, handleLogin, setLoading, cities, getCities, currentUser, getFollowing }) {
 
     return (
         <Bootstrap.Container className="homepage">
             {(isLoggedIn && currentUser!==null)
-                ? <Load setCities={setCities} cities={cities} getCities={getCities} getFollowing={getFollowing} setLoading={setLoading}
-                setFollowing={setFollowing} friendFavorites={friendFavorites} following={following} queryCityFromDB={queryCityFromDB}/>
-                : <LoggedOutView handleLogin={handleLogin}/>
+                ? <Map setLoading={setLoading} cities={cities} getCities={getCities} getFollowing={getFollowing}/>
+                : <LoggedOutView handleLogin={handleLogin} setLoading={setLoading}/>
               }
         </Bootstrap.Container>
     )
