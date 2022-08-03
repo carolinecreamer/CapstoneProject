37a1f5428f44ecc8f049e55d6b3fe35b51413a31
Caroline Creamer <ccreamer@fb.com>
2022-08-02 19:37:04 -0700

    Loading spinner

--- ui/src/components/Load/Load.jsx
+++ ui/src/components/Load/Load.jsx
@@ -12,21 +12,30 @@ import "./Load.css"
 import { set } from "lodash";
 
 export default function Load({ setCities, cities, getCities, getFollowing, setFollowing, following }) {
-    // Get user and city info by calling API wrapper functions in App.jsx, used to load the users saved cities and who the user is following
-    React.useEffect(() => {
-        async function onLoad() {
-            const citiesRes = await getCities();
-            setCities(citiesRes.data.cities);
+  // Get user and city info by calling API wrapper functions in App.jsx, used to load the users saved cities and who the user is following
+  React.useEffect(() => {
+    async function onLoad() {
+      const citiesRes = await getCities();
+      setCities(citiesRes.data.cities);
 
-            const followingRes = await getFollowing();
-            setFollowing(followingRes.data.following)
-        }
+      const followingRes = await getFollowing();
+      setFollowing(followingRes.data.following)
+    }
 
-        onLoad()
-      }, [])
+    onLoad()
+  }, [])
 
 
+  // if cities or following haven't loaded yet, show spinner
+  if (cities == null || following == null) {
     return (
-        <Map cities={cities} following={following}/>
+      <Spinner animation="border" role="status" className="loading">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
     )
+  }
+
+  return (
+    <Map cities={cities} following={following} />
+  )
 }
