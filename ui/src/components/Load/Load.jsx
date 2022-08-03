ca37e13919c32b878d69101f8832e8e23ea39ec8
Caroline Creamer <ccreamer@fb.com>
2022-08-02 19:37:04 -0700

    Loading spinner

--- ui/src/components/Load/Load.jsx
+++ ui/src/components/Load/Load.jsx
@@ -11,20 +11,19 @@ import Spinner from 'react-bootstrap/Spinner';
 import "./Load.css"
 import { set } from "lodash";
 
-export default function Load({ queryCityFromDB, setCities, cities, getCities, getFollowing, setFollowing,friendFavorites, following }) {
-    //const [cities, setCities] = useState(null);
-    React.useEffect(() => {
-        async function onLoad() {
-            const citiesRes = await getCities();
-            setCities(citiesRes.cities);
+export default function Load({ setCities, cities, getCities, getFollowing, setFollowing, following }) {
+  // Get user and city info by calling API wrapper functions in App.jsx, used to load the users saved cities and who the user is following
+  React.useEffect(() => {
+    async function onLoad() {
+      const citiesRes = await getCities();
+      setCities(citiesRes.data.cities);
 
-            const followingRes = await getFollowing();
-            setFollowing(followingRes.following)
-            addToFavMap();
-        }
+      const followingRes = await getFollowing();
+      setFollowing(followingRes.data.following)
+    }
 
-        onLoad()
-      }, [])
+    onLoad()
+  }, [])
 
       function addToFavMap()  {
 
@@ -50,7 +49,20 @@ export default function Load({ queryCityFromDB, setCities, cities, getCities, ge
           )
     }
 
+  // if cities or following haven't loaded yet, show spinner
+  if (cities == null || following == null) {
     return (
+<<<<<<< HEAD
         <Map cities={cities} friendFavorites={friendFavorites} following={following} queryCityFromDB={queryCityFromDB}/>
+=======
+      <Spinner animation="border" role="status" className="loading">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+>>>>>>> 37a1f54 (Loading spinner)
     )
+  }
+
+  return (
+    <Map cities={cities} following={following} />
+  )
 }
